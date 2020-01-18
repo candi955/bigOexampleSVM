52b52f0a4c131fc6e3c1a8cb55f40d0575c52359
candi955 <44098788+candi955@users.noreply.github.com>
2020-01-18 03:27:18 -0700

    Cleaned up coding and wording within program.

--- SVM_MonthlyPredictor_withTkinter.py
+++ SVM_MonthlyPredictor_withTkinter.py
@@ -20,20 +20,15 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-
-
 # pulling excel file and creating variable
 lottoExcel = xlrd.open_workbook('PastWinningNum_SVM_Excel.xlsx')
 # Creating variable to convert excel file to a dataframe (using pandas)
 sheets = lottoExcel.sheets()
 
 
-
 for sheet in sheets:
     lottoSheetData = np.array([[sheet.cell_value(r, c) for c in range(sheet.ncols)] for r in range(sheet.nrows)])
-    #lottoSheetData_DataFrame = pd.DataFrame(lottoSheetData)
-    # print('\n' + '\n' + 'LottoSheet Data, DataFrame(excel) format:')
-    # print(lottoSheetData_DataFrame)
+
 # creating dataframe for tkinter
 df = pd.DataFrame(lottoSheetData)
 
